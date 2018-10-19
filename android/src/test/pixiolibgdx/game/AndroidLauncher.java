3395956e014d30a6ac5e1cde23a512098b96ce0b
Vladyslav Cherednichenko <vladyslav@Vladyslavs-Mac-mini.local>
2018-10-19 11:36:12 +0300

    first commit

---
+++ android/src/test/pixiolibgdx/game/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package test.pixiolibgdx.game;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import test.pixiolibgdx.game.PixioLibgdx;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PixioLibgdx(), config);
+		config.numSamples = 2;
+	}
+}
---
+++ desktop/src/test/pixiolibgdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package test.pixiolibgdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import test.pixiolibgdx.game.PixioLibgdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PixioLibgdx(), config);
+	}
+}
